9b25d377decd48ec5904f4e98a24ddf26cf568b7
Catalina Cáceres <laucata7@gmail.com>
2018-04-17 07:50:36 -0500

    Add files via upload

---
+++ LauraCacere_Ej20.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+//Metodo Adveccion Lineal
+using std::cout;
+using std::endl;
+
+int main{
+	
+	double *u;
+	double dx=0.1, dt=0.01;
+	double x_min=0.0, x_max=2.0;
+	double t=0.5,c=1.0;
+	double n=x_max/dx;
+
+	
+	int funcion=(x_max-x_min)dx+1;
+	
+
+	for (int i=0;int i<n; i++){
+	x=x_min+i*dx;
+	if(x>0.75 && x<=1.25){
+		u[i]=1.0;
+	else
+	
+	
+		
+	
+ 
+	
+
+	
